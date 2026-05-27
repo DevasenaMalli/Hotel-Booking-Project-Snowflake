43c7a49c1d64b1801947c859a0c88442fb7a49a0
DevasenaMalli <168489006+DevasenaMalli@users.noreply.github.com>
2026-05-27 12:25:59 -0700

    Fix table name and column alias in SQL script

--- Processing.sql
+++ Processing.sql
@@ -113,14 +113,14 @@ SELECT * FROM SILVER_HOTEL_BOOKINGS;
 CREATE TABLE GOLD_AGG_DAILY_BOOKING AS
 SELECT 
     check_in_date AS data,
-    COUNT(*) AS total_bookings,
+    COUNT(*) AS total_booking,
     SUM(total_amount) AS total_revenue
 FROM SILVER_HOTEL_BOOKINGS
 GROUP BY check_in_date
 ORDER BY data;
 
 
-CREATE TABLE GILD_AGG_HOTEL_CITY_SALES AS 
+CREATE TABLE GOLD_AGG_HOTEL_CITY_SALES AS 
 SELECT
     hotel_city,
     SUM(total_amount) AS total_revenue
